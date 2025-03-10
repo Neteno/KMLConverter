3d3fdd29ed1d6b338e57c20ecb4c1eb76d245efc
Neteno <szymon.andrzejczak@spoko.pl>
2025-03-10 10:20:13 +0100

    Update project files and cleanup

--- KmlConverter/Form1.Designer.cs
+++ KmlConverter/Form1.Designer.cs
@@ -7,12 +7,13 @@
         private System.Windows.Forms.Button btnSelectFile;
         private System.Windows.Forms.Button btnConvert;
         private System.Windows.Forms.Label lblStatus;
-        private System.Windows.Forms.NumericUpDown numericUpDownCoords;
         private System.Windows.Forms.ComboBox comboBoxBrackets;
         private System.Windows.Forms.ComboBox comboBoxSeparator;
+        private System.Windows.Forms.ComboBox comboBoxCoords;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
 
         protected override void Dispose(bool disposing)
         {
@@ -29,13 +30,13 @@
             this.btnSelectFile = new System.Windows.Forms.Button();
             this.btnConvert = new System.Windows.Forms.Button();
             this.lblStatus = new System.Windows.Forms.Label();
-            this.numericUpDownCoords = new System.Windows.Forms.NumericUpDown();
             this.comboBoxBrackets = new System.Windows.Forms.ComboBox();
             this.comboBoxSeparator = new System.Windows.Forms.ComboBox();
+            this.comboBoxCoords = new System.Windows.Forms.ComboBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownCoords)).BeginInit();
+            this.label4 = new System.Windows.Forms.Label();
             this.SuspendLayout();
 
             this.txtFilePath.Location = new System.Drawing.Point(12, 12);
@@ -47,19 +48,18 @@
             this.btnSelectFile.Text = "Wybierz plik";
             this.btnSelectFile.Click += new System.EventHandler(this.btnSelectFile_Click);
 
-            this.btnConvert.Location = new System.Drawing.Point(320, 130);
+            this.btnConvert.Location = new System.Drawing.Point(320, 170);
             this.btnConvert.Size = new System.Drawing.Size(100, 30);
             this.btnConvert.Text = "Konwertuj";
             this.btnConvert.Click += new System.EventHandler(this.btnConvert_Click);
 
-            this.lblStatus.Location = new System.Drawing.Point(12, 170);
+            this.lblStatus.Location = new System.Drawing.Point(12, 210);
             this.lblStatus.Size = new System.Drawing.Size(400, 20);
             this.lblStatus.Text = "Status: Oczekiwanie na wybór pliku";
 
-            this.numericUpDownCoords.Location = new System.Drawing.Point(150, 50);
-            this.numericUpDownCoords.Minimum = 1;
-            this.numericUpDownCoords.Maximum = 3;
-            this.numericUpDownCoords.Value = 2;
+            this.comboBoxCoords.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxCoords.Location = new System.Drawing.Point(150, 50);
+            this.comboBoxCoords.Items.AddRange(new object[] { "X", "Y", "Z", "X,Y", "X,Z", "Y,Z", "X,Y,Z" });
 
             this.comboBoxBrackets.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.comboBoxBrackets.Location = new System.Drawing.Point(150, 90);
@@ -67,11 +67,11 @@
 
             this.comboBoxSeparator.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.comboBoxSeparator.Location = new System.Drawing.Point(150, 130);
-            this.comboBoxSeparator.Items.AddRange(new object[] { "Nowa linia", "Spacja" });
+            this.comboBoxSeparator.Items.AddRange(new object[] { "Nowa linia", "Spacja", ", + Nowa linia"});
 
             this.label1.Location = new System.Drawing.Point(12, 50);
             this.label1.Size = new System.Drawing.Size(120, 20);
-            this.label1.Text = "Liczba współrzędnych:";
+            this.label1.Text = "Wybór współrzędnych:";
 
             this.label2.Location = new System.Drawing.Point(12, 90);
             this.label2.Size = new System.Drawing.Size(120, 20);
@@ -81,23 +81,26 @@
             this.label3.Size = new System.Drawing.Size(120, 20);
             this.label3.Text = "Separator współrzędnych:";
 
-            this.ClientSize = new System.Drawing.Size(440, 200);
+            this.label4.Location = new System.Drawing.Point(12, 170);
+            this.label4.Size = new System.Drawing.Size(120, 20);
+            this.label4.Text = "Przetwarzanie:";
+
+            this.ClientSize = new System.Drawing.Size(440, 240);
             this.Controls.Add(this.txtFilePath);
             this.Controls.Add(this.btnSelectFile);
             this.Controls.Add(this.btnConvert);
             this.Controls.Add(this.lblStatus);
-            this.Controls.Add(this.numericUpDownCoords);
+            this.Controls.Add(this.comboBoxCoords);
             this.Controls.Add(this.comboBoxBrackets);
             this.Controls.Add(this.comboBoxSeparator);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
             this.Text = "KML to CSV Converter";
 
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownCoords)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
         }
     }
-
 }
--- KmlConverter/Form1.cs
+++ KmlConverter/Form1.cs
@@ -3,8 +3,7 @@ using System.Linq;
 using System.Windows.Forms;
 using System.Xml.Linq;
 using System.IO;
-using System.Globalization;
-using CsvHelper;
+using System.Collections.Generic;
 
 namespace KmlConverter
 {
@@ -15,6 +14,8 @@ namespace KmlConverter
             InitializeComponent();
             comboBoxBrackets.SelectedIndex = 0;
             comboBoxSeparator.SelectedIndex = 0; // Domyœlnie nowa linia
+            comboBoxCoords.Items.AddRange(new string[] { "X", "Y", "Z", "X,Y", "X,Z", "Y,Z", "X,Y,Z" });
+            comboBoxCoords.SelectedIndex = 3; // Domyœlnie "X,Y"
         }
 
         private void btnSelectFile_Click(object sender, EventArgs e)
@@ -47,43 +48,98 @@ namespace KmlConverter
                 XNamespace ns = "http://www.opengis.net/kml/2.2";
                 XDocument kml = XDocument.Load(kmlPath);
 
-                int coordinateCount = (int)numericUpDownCoords.Value;
                 string selectedBrackets = comboBoxBrackets.SelectedItem?.ToString() ?? "[]";
                 string openBracket = selectedBrackets.Substring(0, 1);
                 string closeBracket = selectedBrackets.Substring(1, 1);
-                string separator = comboBoxSeparator.SelectedItem.ToString() == "Nowa linia" ? Environment.NewLine : " ";
+                string selectedFormat = comboBoxCoords.SelectedItem.ToString();
+
+                string separator;
+                switch (comboBoxSeparator.SelectedItem.ToString())
+                {
+                    case "Nowa linia":
+                        separator = Environment.NewLine;
+                        break;
+                    case "Spacja":
+                        separator = " ";
+                        break;
+                    case ", + Nowa linia":
+                        separator = "," + Environment.NewLine;
+                        break;
+                    case ";":
+                        separator = ";";
+                        break;
+                    default:
+                        separator = Environment.NewLine;
+                        break;
+                }
 
                 var placemarks = kml.Descendants().Where(e => e.Name.LocalName == "Placemark")
                     .SelectMany(p => FormatCoordinates(
                         p.Descendants().FirstOrDefault(e => e.Name.LocalName == "coordinates")?.Value.Trim() ?? "",
-                        coordinateCount, openBracket, closeBracket
-                    )).ToList();
+                        selectedFormat, openBracket, closeBracket
+                    )).Where(line => !string.IsNullOrWhiteSpace(line)).ToList();
 
+                // Zapisujemy do pliku CSV
                 using (var writer = new StreamWriter(csvPath))
                 {
                     writer.Write(string.Join(separator, placemarks));
                 }
+
+                // Usuwamy puste nawiasy po zapisaniu pliku
+                RemoveEmptyBrackets(csvPath, openBracket, closeBracket);
 
                 lblStatus.Text = "Konwersja zakoñczona! Plik zapisany jako: " + csvPath;
                 MessageBox.Show("Konwersja zakoñczona!", "Sukces", MessageBoxButtons.OK, MessageBoxIcon.Information);
             }
             catch (Exception ex)
             {
-                lblStatus.Text = "B³¹d: " + ex.Message;
+                MessageBox.Show("B³¹d: " + ex.Message, "B³¹d", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                comboBoxCoords.SelectedIndex = 3; // Resetujemy do domyœlnego "X,Y"
             }
         }
 
-        private IEnumerable<string> FormatCoordinates(string rawCoordinates, int count, string openBracket, string closeBracket)
+        private IEnumerable<string> FormatCoordinates(string rawCoordinates, string selectedFormat, string openBracket, string closeBracket)
         {
             return rawCoordinates.Split(' ')
                 .Select(coord =>
                 {
                     var values = coord.Split(',');
-                    if (values.Length < count) return null;
-                    return openBracket + string.Join(",", values.Take(count)) + closeBracket;
+                    int availableCoords = values.Length;
+
+                    if (availableCoords == 0)
+                        return null; // Ignoruj puste wiersze
+
+                    List<string> selectedValues = new List<string>();
+
+                    // Sprawdzamy, które wspó³rzêdne nale¿y dodaæ na podstawie wybranego formatu
+                    if (selectedFormat.Contains("X") && availableCoords >= 1)
+                        selectedValues.Add(values[0]);
+                    if (selectedFormat.Contains("Y") && availableCoords >= 2)
+                        selectedValues.Add(values[1]);
+                    if (selectedFormat.Contains("Z") && availableCoords >= 3)
+                        selectedValues.Add(values[2]);
+
+                    // Jeœli ¿adna wartoœæ nie zosta³a dodana, zwróæ null (czyli ignoruj ten punkt)
+                    if (selectedValues.Count == 0)
+                        return null;
+
+                    // Zwracamy poprawnie sformatowane wspó³rzêdne w nawiasach
+                    return openBracket + string.Join(",", selectedValues) + closeBracket;
                 })
-                .Where(coord => coord != null);
+                .Where(coord => coord != null); // Ignorujemy null (czyli puste wspó³rzêdne)
+        }
+
+        private void RemoveEmptyBrackets(string filePath, string openBracket, string closeBracket)
+        {
+            // Wczytujemy plik CSV
+            var lines = File.ReadAllLines(filePath).Where(line =>
+            {
+                // Sprawdzamy, czy linia nie zawiera pustych nawiasów
+                return !line.Contains(openBracket + closeBracket); // Ignorujemy linie zawieraj¹ce puste nawiasy
+            }).ToList();
+
+            // Zapisujemy z powrotem do pliku bez pustych nawiasów
+            File.WriteAllLines(filePath, lines);
         }
     }
-
 }
